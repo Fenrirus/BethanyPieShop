01aaeaeba665e8954d31cc4619a67c4979f285e1
Marian Marian <fenrir55@gmail.com>
2020-04-16 20:52:24 +0200

    Routing

--- BethanyPieShop/Controlers/PieController.cs
+++ BethanyPieShop/Controlers/PieController.cs
@@ -26,5 +26,18 @@ namespace BethanyPieShop.Controlers
             piesListViewModel.CurrentCategory = "Serniczki";
             return View(piesListViewModel);
         }
+
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+            if (pie == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return View(pie);
+            }
+        }
     }
 }
